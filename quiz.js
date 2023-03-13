112d1cfcc51beff39a0a9890b1cf8af51c06825d
Yileri <yigit.ileri@ozu.edu.tr>
2023-03-13 10:25:18 +0300

    step by step

---
+++ quiz.js
@@ -0,0 +1,9 @@
+<script>
+    var quiz = [
+        {question:"10-2", answer:8, studentAnswer:};
+        {question:"10-2", answer:8, studentAnswer:};
+        {question:"10-2", answer:8, studentAnswer:};
+        {question:"10-2", answer:8, studentAnswer:};
+        {question:"10-2", answer:8, studentAnswer:};
+    ];
+</script>
