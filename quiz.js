1d55c23c7ddefcd9c60cc6bae89ad58aea2d211a
Yileri <yigit.ileri@ozu.edu.tr>
2023-03-13 19:18:18 +0300

    added questions

--- quiz.js
+++ quiz.js
@@ -1,13 +1,13 @@
-/*
-var quiz = [
-    {question:"10-2", answer:8, studentAnswer:};        {question:"10-2", answer:8, studentAnswer:};
-    {question:"10-2", answer:8, studentAnswer:};
-    {question:"10-2", answer:8, studentAnswer:};
-    {question:"10-2", answer:8, studentAnswer:};
-    ];
-*/
+let qNumber = 0;
+let quiz = [
+    {question: "What is 10-2=?", answer:8, studentAnswer: 0},
+    {question: "What is 22-5=?", answer:17, studentAnswer: 0},
+    {question: "What is 7-3=?", answer:4, studentAnswer: 0},
+    {question: "What is 15-14=?", answer:1, studentAnswer: 0},
+    {question: "What is 13-4=?", answer:9, studentAnswer: 0}
+];
 
-function display() {
-    alert("SÜÜÜ");
+function check() {
+    alert(quiz[0].question);
 }
 
